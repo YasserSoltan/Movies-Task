9b162de603c68770bc870defd34dfea0a57830f6
YasserSoltan <yassersoltan64@gmail.com>
2026-01-16 18:07:06 +0200

    fix  main page style

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-700 font-sans dark:bg-black">
-      <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-100">
+      <h1 className="text-4xl font-bold text-white dark:text-zinc-100">
         Welcome to Movies!
       </h1>
       <Link
         href="/movies"
-        className="mt-4 rounded-lg bg-blue-600 px-6 py-3 text-white transition-colors hover:bg-blue-700"
+        className="mt-4 rounded-lg bg-green-800 px-6 py-3 text-white transition-colors hover:bg-green-700"
       >
         Go to Movies Page
       </Link>
